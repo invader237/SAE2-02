70f26f24194bfbd39c49454108d4a1434afd898e
invader237 <diego.trivino.fr@gmail.com>
2024-06-09 20:11:52 +0200

    :fire:(fileManagement.ts): suppression d'une fonction inutile'

--- fileManagement.ts
+++ fileManagement.ts
@@ -1,30 +1,4 @@
-async saveGraphToFile(filePath: string): Promise < void> {
-    if(!filePath) {
-        throw new Error("Le chemin du fichier n'est pas spécifié.");
-    }
-    let data = '';
-    let edgeCount = 0;
-    const sortedVertices = Array.from(this.adjacencyList.keys()).sort((a, b) => a - b);
-    sortedVertices.forEach(source => {
-        const edges = this.adjacencyList.get(source);
-        if (edges) {
-            edges.forEach((weight, destination) => {
-                data += `${source} ${destination} ${weight}\n`;
-                edgeCount++;
-            });
-        }
-    });
-    const header = `${this.getVertexCount()} ${edgeCount}\n`;
-    data = header + data;
-
-    try {
-        await Deno.writeTextFile(filePath, data);
-        console.log("Graphe sauvegardé au format liste d'adjacence avec succès.");
-    } catch(error) {
-        console.error("Erreur lors de la sauvegarde du graphe:", error);
-        throw error;
-    }
-}
+import { Graph } from "./class_graph";
 
 async function loadGraphFromFile(filePath: string): Promise<Graph> {
     const graph = new Graph();
@@ -82,4 +56,3 @@ async function loadGraphMenu(): Promise<void> {
 }
 
 export { loadGraphMenu };
-export { saveGraphToFile };
