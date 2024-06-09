7231fb5b6558e1ce18c053108e56623e1d12a06c
Mathieu Kuhnapfel <e9486u@iutm-inf-f33-10.iutmetz.site.univ-lorraine.fr>
2024-06-09 21:50:24 +0200

    :fire:(fileManagement.ts): suppression de fonction

--- fileManagement.ts
+++ fileManagement.ts
@@ -32,27 +32,3 @@ async function loadGraphFromFile(filePath: string): Promise<Graph> {
     });
     return graph;
 }
-
-async function loadGraphMenu(): Promise<void> {
-    while (true) {
-        console.log("Veuillez entrer le chemin complet du fichier du graphe:");
-        const filePath = await prompt("Chemin du fichier :");
-        try {
-            const graph = await loadGraphFromFile(filePath);
-            console.log("Graphe chargé avec succès.");
-            //await graphOptions(graph);
-            break;
-        } catch (error) {
-            console.error("Erreur lors du chargement du graphe. Vérifiez le chemin du fichier et réessayez.", error);
-            console.log("1. Réessayer");
-            console.log("2. Retourner au menu principal");
-            const choice = await prompt("Choisissez une option:");
-            if (choice === '2') {
-                //menue principal 
-                return;
-            }
-        }
-    }
-}
-
-export { loadGraphMenu };
